8874cd139bad490a9e05d3eaa0e3af01d261404a
Marcus Weiner <marcus.weiner@gmail.com>
2024-04-28 02:00:10 +0200

    Update backend

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "http" {
-    address        = "https://ffddorf-terraform-backend.fly.dev/state/playground/dev"
-    lock_address   = "https://ffddorf-terraform-backend.fly.dev/state/playground/dev"
-    unlock_address = "https://ffddorf-terraform-backend.fly.dev/state/playground/dev"
+    address        = "https://ffddorf-terraform-backend.fly.dev/state/terraform-playground/dev"
+    lock_address   = "https://ffddorf-terraform-backend.fly.dev/state/terraform-playground/dev"
+    unlock_address = "https://ffddorf-terraform-backend.fly.dev/state/terraform-playground/dev"
     username       = "github_pat"
   }
 }
